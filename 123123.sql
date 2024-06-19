4285ef89fbbc2592de2f07f796e976411b70363f
Kyrliyki <109131152+Kyrliyki@users.noreply.github.com>
2024-06-19 14:45:21 +0400

    Add files via upload

--- 123123.sql
+++ 123123.sql
@@ -21,88 +21,123 @@ USE `TestTaskDB`;
 
 -- Дамп структуры для таблица TestTaskDB.algebra
 CREATE TABLE IF NOT EXISTS `algebra` (
-  `id` int DEFAULT NULL,
-  `id_user` int DEFAULT NULL,
-  `05.01` int DEFAULT NULL,
-  `08.02` int DEFAULT NULL,
-  `09.02` int DEFAULT NULL,
-  `23.02` int DEFAULT NULL,
-  `01.03` int DEFAULT NULL,
-  `25.03` int DEFAULT NULL,
-  `02.04` int DEFAULT NULL,
-  `20.04` int DEFAULT NULL,
-  `27.05` int DEFAULT NULL,
-  `13.06` int DEFAULT NULL,
-  `12.07` int DEFAULT NULL,
-  `30.08` int DEFAULT NULL,
-  `09.09` int DEFAULT NULL,
-  `07.12` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_users` int DEFAULT NULL,
+  `05.01` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `09.02` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `23.02` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `08.02` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `01.03` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `25.03` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `02.04` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `20.04` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `27.05` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `13.06` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `12.07` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `30.08` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `09.09` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `07.12` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `17.07` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  KEY `Индекс 1` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы TestTaskDB.algebra: ~0 rows (приблизительно)
+-- Экспортируемые данные не выделены.
 
 -- Дамп структуры для таблица TestTaskDB.Biology
 CREATE TABLE IF NOT EXISTS `Biology` (
-  `id` int NOT NULL,
-  `id_user` int DEFAULT NULL,
-  `14.01` int DEFAULT NULL,
-  `18.01` int DEFAULT NULL,
-  `25.01` int DEFAULT NULL,
-  `12.02` int DEFAULT NULL,
-  `25.03` int DEFAULT NULL,
-  `13.05` int DEFAULT NULL,
-  `20.05` int DEFAULT NULL,
-  `29.05` int DEFAULT NULL,
-  `25.06` int DEFAULT NULL,
-  `17.07` int DEFAULT NULL,
-  `30.08` int DEFAULT NULL,
-  `10.09` int DEFAULT NULL,
-  `18.09` int DEFAULT NULL,
-  `12.10` int DEFAULT NULL,
-  `09.11` int DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_users` int DEFAULT NULL,
+  `07.01` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `28.01` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `23.02` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `07.03` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `13.03` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `29.03` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `07.04` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `10.04` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `22.04` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `19.07` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `21.07` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `12.09` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `27.10` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `22.05` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `02.06` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы TestTaskDB.Biology: ~0 rows (приблизительно)
+-- Экспортируемые данные не выделены.
 
 -- Дамп структуры для таблица TestTaskDB.Chemistry
 CREATE TABLE IF NOT EXISTS `Chemistry` (
-  `id` int NOT NULL,
-  `id_user` int DEFAULT NULL,
-  `07.01` int DEFAULT NULL,
-  `28.01` int DEFAULT NULL,
-  `23.02` int DEFAULT NULL,
-  `07.03` int DEFAULT NULL,
-  `13.03` int DEFAULT NULL,
-  `29.03` int DEFAULT NULL,
-  `07.04` int DEFAULT NULL,
-  `10.04` int DEFAULT NULL,
-  `22.04` int DEFAULT NULL,
-  `19.07` int DEFAULT NULL,
-  `21.07` int DEFAULT NULL,
-  `12.09` int DEFAULT NULL,
-  `27.10` int DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_users` int DEFAULT NULL,
+  `14.01` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `18.01` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `25.01` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `12.02` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `25.03` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `13.05` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `20.05` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `29.05` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `25.06` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `17.07` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `30.08` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `10.09` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `18.09` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `12.10` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `09.11` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы TestTaskDB.Chemistry: ~0 rows (приблизительно)
+-- Экспортируемые данные не выделены.
 
 -- Дамп структуры для таблица TestTaskDB.History
 CREATE TABLE IF NOT EXISTS `History` (
-  `id` int NOT NULL,
-  `id_user` int DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_users` int DEFAULT NULL,
+  `03.01` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `05.01` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `20.01` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `14.02` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `20.03` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `10.05` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `02.06` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `15.07` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `26.07` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `01.08` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `28.08` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `26.10` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `09.11` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `06.12` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `18.12` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы TestTaskDB.History: ~0 rows (приблизительно)
+-- Экспортируемые данные не выделены.
 
 -- Дамп структуры для таблица TestTaskDB.rus
 CREATE TABLE IF NOT EXISTS `rus` (
-  `id` int DEFAULT NULL,
-  `id_users` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_users` int DEFAULT NULL,
+  `11.01` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `23.02` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `29.02` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `25.03` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `08.04` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `02.05` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `09.06` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `30.06` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `21.07` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `16.08` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `10.09` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `12.09` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `20.09` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `08.11` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `28.11` char(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  KEY `Индекс 1` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы TestTaskDB.rus: ~0 rows (приблизительно)
+-- Экспортируемые данные не выделены.
 
 -- Дамп структуры для таблица TestTaskDB.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -121,16 +156,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `History` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы TestTaskDB.users: ~5 rows (приблизительно)
-INSERT INTO `users` (`id`, `username`, `password`, `status`, `surname`, `firstname`, `patronymic`, `dob`, `algebra`, `russian_language`, `Biology`, `Chemistry`, `History`) VALUES
-	(1, 'direktor', '$2y$10$o0YFaVXIdNDg/nRLmVxNROF6hOS2QONfK0y.C8PaKcRuGnlzZdVQ6', 'admin', '', '', '', '0000-00-00', 0, 0, 0, 0, 0),
-	(4, 'ilnur', '$2y$10$CR9I9velbReqFfXodS2bZ.tkWmjvVuizMvJ/M4CoCrLjcqde4.D42', 'student', 'Салахов', 'Илнур', 'Рамисович', '2002-09-23', 0, 0, 0, 0, 0),
-	(7, 'alex', '$2y$10$WW1B7oWkWo7DHxoGCbOqrObc6.3nQ7nVBVsKdVl.ReynR7DRRyQCm', 'student', 'Калинин', 'Александр', 'Константинович', '2001-08-10', 0, 0, 0, 0, 0),
-	(8, 'polina', '$2y$10$rcQyGHhaO3c6y69BjgNaCeB/48b6j3/5C6qLKOJY0x1drujpSqL7u', 'student', 'Андреева', 'Полина', '', '2001-05-09', 0, 0, 0, 0, 0),
-	(9, 'sasha', '$2y$10$4.arXdDHe8QByjZ92ZyOPu7zBVseZ2.1LT9w5rQiMr.jWui42cshO', 'student', 'shelemetev', 'sasha', 'olegovich', '2004-02-19', 0, 0, 0, 0, 0),
-	(15, 'teacher', '$2y$10$6j9nc6Vkwoui8F354Gz57.csp59rI4ehIJtM9g0FOWn79bu7ZxpcK', 'teacher', 'teacher', 'teacher', 'teacher', '2024-06-17', 1, 1, 0, 0, 1);
+-- Экспортируемые данные не выделены.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
